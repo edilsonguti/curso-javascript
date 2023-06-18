2daf400738e4b71b2643d625c99b338d956aa5ca
edilsonguti <131001164+edilsonguti@users.noreply.github.com>
2023-06-18 01:20:48 -0300

    Create ex009.js

---
+++ aula11/ex009.js
@@ -0,0 +1,17 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
+
+
+
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log('Você é Estrangeiro')
+} else {
+    console.log('Você é Brasileiro!')
+}

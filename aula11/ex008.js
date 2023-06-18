9ce742d62cfb222fc990f25af3383e948d048d8c
edilsonguti <131001164+edilsonguti@users.noreply.github.com>
2023-06-18 01:20:42 -0300

    criado ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(` A velocidade de seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples!
+    console.log('Você ultrapassou a velocidade permitada. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
